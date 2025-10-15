9f11b30380b14c0ebe22d525158cc6c769b050a6
lemonnik <lemongamer6484@gmail.com>
2025-10-15 19:22:58 +0200

    init

---
+++ src/main/java/dev/lemonnik/bunker/BunkerMain.java
@@ -0,0 +1,24 @@
+package dev.lemonnik.bunker;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BunkerMain.MODID)
+public class BunkerMain {
+    public static final String MODID = "bunker";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    
+    public BunkerMain(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Adding bunker!");
+    }
+}
